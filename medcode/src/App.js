414127a3f3993aa10315cb4103fd8eda05f07f39
Paarthbathla338 <Paarth.bathla@outlook.com>
2021-08-13 18:15:30 +0530

    Basic App working

---
+++ medcode/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+    <h1>Lets Build some Cool Shit</h1>
+    {/*Header*/}
+    {/*Home*/}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ medcode/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB_EQ9c8RaovrtPWiEu9XpGIUNZ_xI1Fdg",
+    authDomain: "medcode-338.firebaseapp.com",
+    projectId: "medcode-338",
+    storageBucket: "medcode-338.appspot.com",
+    messagingSenderId: "6357501243",
+    appId: "1:6357501243:web:61485f887ec195c64aa31e",
+    measurementId: "G-GT66QRCFLL"
+  };
