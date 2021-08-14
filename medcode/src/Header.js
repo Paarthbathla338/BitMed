fa2481d345bc2df48bd81af56616bfeb8334d17b
Paarthbathla338 <Paarth.bathla@outlook.com>
2021-08-14 14:35:39 +0530

    Header Completed

--- medcode/src/Header.js
+++ medcode/src/Header.js
@@ -1,31 +1,41 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
 
 function Header() {
   return (
     <div className="header">
-      <img className="header_logo" src="https://img.freepik.com/free-vector/cute-happy-smiling-medicine-pill-capsule-bottle-flat-cartoon-character-illustration-icon-isolated-white-tablet-pill-capsule-antibiotic-healthcare-drug-medical_92289-526.jpg?size=626&ext=jpg" alt="" />
+      <img className="header_logo" src="https://medicalaid.org/wp-content/uploads/sb-instagram-feed-images/internationalmedicalaid.jpg" alt="" />
 
       <div className="header_search">
-        <input className="header_search" type="text"></input>
+        <input className="header_searchInput" type="text"></input>
+        <SearchIcon className="header_searchIcon"/>
       </div>
 
       <div className="header_nav">
         <div className="header_option">
-          <span className="header_optionlineOne">Hello Guest </span>
+          <span className="header_optionlineOne">Hello Guest, </span>
           <span className="header_optionlineTwo">Sign In</span>
         </div>
 
         <div className="header_option">
           <span className="header_optionlineOne">Returns </span>
-          <span className="header_optionlineTwo">and Orders</span>
+          <span className="header_optionlineTwo"> & Orders</span>
         </div>
 
         <div className="header_option">
-          <span className="header_optionlineOne">Your </span>
-          <span className="header_optionlineTwo">Prime</span>
+          <span className="header_optionlineOne">Get </span>
+          <span className="header_optionlineTwo">Vaccinated</span>
         </div>
       </div>
+      <div className="header_optionBasket">
+        <ShoppingBasketIcon/>
+        <span className="header_optionlineTwo header_basketCount">0</span>
+  
+      </div>
+
     </div>
   );
 }
