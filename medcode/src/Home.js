9fd5c52f11523f133e28ec7a4903ef7a59c8a68b
Paarthbathla338 <Paarth.bathla@outlook.com>
2021-08-14 18:19:31 +0530

    Added a Card and STYLED it

--- medcode/src/Home.js
+++ medcode/src/Home.js
@@ -9,9 +9,15 @@ function Home() {
                 <a href="https://www.cowin.gov.in/" target="_blank" rel="noreferrer"><img className="home_image" src="https://images.indianexpress.com/2021/02/vaccine-8.jpg" alt="" /></a>
 
             </div>
+            
+            <div className="home_main">
+            <p className="ap py-5">OUR PRODUCTS,</p>
+
 
         <div className="home_row">
-            <Product title="Paracetamol" price="72" image="https://media.istockphoto.com/photos/packet-of-generic-paracetamol-tablets-picture-id1022216070?k=6&m=1022216070&s=612x612&w=0&h=dKRICd0TkhnikMaw23rPI5QvwmmCrFdcJ2uYuwhP8Cg="/>
+            <Product title="Paracetamol 500mg Tablets(Each tablet contains Paracetamol 500 mg.)" price="72" image="https://media.istockphoto.com/photos/packet-of-generic-paracetamol-tablets-picture-id1022216070?k=6&m=1022216070&s=612x612&w=0&h=dKRICd0TkhnikMaw23rPI5QvwmmCrFdcJ2uYuwhP8Cg="/>
+            <Product/>
+            <Product/>
             <Product/>
    
 
@@ -22,17 +28,23 @@ function Home() {
             <Product/>
             <Product/>
             <Product/>
+            <Product/>
 
         </div>
         
         <div className="home_row">
 
         <Product/>
+        <Product/>
+        <Product/>
+        <Product/>
+
            
 
         </div>
 
             
+        </div>
         </div>
     )
 }
